06b1224fefce8acbfef6fd578e14ac9ce31470c8
Leandro Beretta <lea.beretta@gmail.com>
2020-06-26 12:50:13 -0300

    log duration

--- mimik.py
+++ mimik.py
@@ -2,10 +2,25 @@ from flask import Flask, request
 
 import requests
 import os
+import time
 
 app = Flask(__name__)
 
+def log_time(method):
+    def duration(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        
+        print('%r - response_time: %2.2f ms' % (method.__name__, (te - ts) * 1000))
+        
+        return result
+    
+    return duration
+
+
 @app.route('/<path>')
+@log_time
 def get(path): # pragma: no cover
     return mimik(path, 
                  request.headers, 
